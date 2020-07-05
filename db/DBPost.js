e79d8ea46fbde0e7342e6834c022d0776bb0b97e
fengjw1 <fengjwxx@163.com>
2020-07-06 00:16:17 +0800

    "finish 6-3"

--- db/DBPost.js
+++ db/DBPost.js
@@ -1,6 +1,7 @@
 class DBPost{
-    constructor(url){
+    constructor(postId){
         this.storageKeyName = 'postList';
+        this.postId = postId;
     }
 
     getAllPostData(){
@@ -14,6 +15,20 @@ class DBPost{
 
     execSetStorageSync(data){
         wx.setStorageSync(this.storageKeyName, data)
+    }
+
+    getPostItemById(){
+        var postsData = this.getAllPostData();
+        var len = postsData.length;
+        for (let i = 0; i < len; i++) {
+            if (postsData[i].postId == this.postId) {
+                return{
+                    index: i,
+                    data:postsData[i]
+                }
+            }
+            
+        }
     }
 
 }
--- pages/post/post.js
+++ pages/post/post.js
@@ -21,6 +21,14 @@ Page({
         })
     },
 
+    onTapToDetail(event){
+        var postId = event.currentTarget.dataset.postId;
+        console.log("postId>>" + postId);
+        wx.navigateTo({
+          url: 'post-detail/post-detail?id=' + postId,
+        })
+    },
+
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
